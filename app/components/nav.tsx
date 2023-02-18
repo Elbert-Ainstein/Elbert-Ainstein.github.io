0dd5a7b5df3f159e1a30efee5e53d7f4d5c580c3
Elbert-Ainstein <iwu48@yahoo.com>
2023-02-18 15:20:48 +0800

    chad

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -33,38 +33,39 @@ export default function Nav() {
         nav.current.style.transform = "translateY(-100%)";
         nav.current.style.transition= "transform 800ms ease-in, opacity 500ms ease-in-out";
         nav.current.style.opacity="0";
-
+        navPattern.current.style.opacity="0";
+        navPattern.current.style.transition="transform 1600ms ease-in, opacity 200ms ease-out, background-size 800ms ease, background-position 800ms ease"
         setState(false);
     }
 
     return (
         <>
             <div className="nav">
-                <Link href="/" className="btn btn-ghost z-10 absolute left-0 pl-24 pt-20 ">
+                <Link href="/" className="btn cursor-none btn-ghost z-10 absolute left-0 pl-24 pt-20 ">
                     <p className="text-white">
                         Elbert Ainstein
                     </p>
                 </Link>
-                <div className="z-10 w-20 absolute top-0 right-0 mr-24 pt-16 text-white">
+                <div className="z-10 w-20 absolute cursor-none top-0 right-0 mr-24 pt-16 text-white">
                     {
                         state == false ? <p 
                         onClick={
                             openNav
                         }
                         // onClick={(ev) => setState(true)} 
-                        className=" btn z-10 btn-ghost ">
+                        className=" btn z-10 btn-ghost cursor-none rounded-none hover:bg-white/10">
                             menu
                         </p> : <p 
                         // onClick={(ev) => setState(false)}
                         onClick={closeNav} 
-                        className=" btn btn-ghost z-10">
+                        className=" btn btn-ghost z-10 cursor-none rounded-none hover:bg-white/10">
                             close
                         </p>
                     }
                 </div>
         
                 {/* { state == 1 ?  */}
-                    <div id="menu" className={`-top-full ${state == true ? "delay-200" : null}`} ref={nav}>
+                    <div id="menu" className={`-top-full delay-200`} ref={nav}>
                         <div id="menu-items" className="font-poppins">
                             <Link href="/" data-type="link" className=" interactable menu-item font-poppins">Home</Link>
                             <Link href="/" data-type="link" className=" interactable menu-item font-nova">Works</Link>
